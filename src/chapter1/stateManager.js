5bde54716351c26f1901b3be61d3ccbf8530b189
Paul Heintzelman <pheintzelman@gmail.com>
2017-02-10 22:00:25 -0800

    added states

---
+++ src/chapter1/stateManager.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var stateManager = {};
+
+stateManager.States = {'Apartment1':'Apartment1', 
+                       'Apartment2':'Apartment2', 
+                       'Apartment3':'Apartment3',
+                       'Hall':'Hall',
+                       'DoomHall':'DoomHall',
+                       'Stairwell':'Stairwell',
+                       'Roof':'Roof',
+                       'Helicopter':'Helicopter'
+                    };
+
+module.exports = stateManager;
